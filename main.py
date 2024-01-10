22ab0d955a22de2c630c8553bea2f215ac971ea2
unknown <rubencavalcanti@gmail.com>
2024-01-10 13:58:25 -0300

    correção arquitetura do projeto

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+import random
+import csv
+from scraping import scrap_google_shopping
+
+def main():
+    # ... Restante do código ...
+
+    # Chama a função de scraping do Google Shopping
+    scrap_google_shopping()
+
+    # Exibe a quantidade total de páginas puxadas
+    print(f"Finalizado")
+
+if __name__ == "__main__":
+    main()
